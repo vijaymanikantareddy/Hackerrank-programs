5cc5a494a645b01af7648b24cf303294da769565
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-05-17 16:12:51 +0530

    Create Bitwise Operators.c

---
+++ Bitwise Operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+int main()
+{
+    int n,k,i,j,and=0,or=0,xor=0,a,b,c;
+    scanf("%d %d",&n,&k);
+    for(i=1;i<n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            a=i&j;
+           if(a>=and && a<k)
+                and=i&j; 
+            b=i|j;
+            if(b>=or && b<k)
+                or=i|j;
+            c=i^j;
+            if(c>=xor && c<k)
+                xor=i^j;
+        }
+    }
+    printf("%d\n%d\n%d",and,or,xor);
+}

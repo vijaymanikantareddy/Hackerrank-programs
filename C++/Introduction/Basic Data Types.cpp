3744ee33ec4e6a8a66322b61620a492b343bdbdc
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-11-11 19:56:19 +0530

    Create Basic Data Types.cpp

---
+++ C++/Introduction/Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<fixed<<endl;
+    cout<<d<<endl;
+    cout<<e<<fixed<<endl;
+    return 0;
+}

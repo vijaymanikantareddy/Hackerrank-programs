6600d8058e0f41a6a1fd2dc3889371f9945511de
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-11-11 19:57:35 +0530

    Create Input and Output.cpp

---
+++ C++/Introduction/Input and Output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c,sum;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum; 
+    return 0;
+}

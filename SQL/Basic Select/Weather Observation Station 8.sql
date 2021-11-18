4e9524383ac74ea7f8ca7a1c6d6871c17f7fe23f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-11-18 22:04:22 +0530

    Update Weather Observation Station 8.sql

--- SQL/Basic Select/Weather Observation Station 8.sql
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -1,3 +1,3 @@
 select distinct city from station
-where substr(city, 1, 1) in ('a','e','i','o','u','A','E','I','O','U'); and 
+where substr(city, 1, 1) in ('a','e','i','o','u','A','E','I','O','U') and 
 substr(city, length(City), 1) in ('a','e','i','o','u','A','E','I','O','U');

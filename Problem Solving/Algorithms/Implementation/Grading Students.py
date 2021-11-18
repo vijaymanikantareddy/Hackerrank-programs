5003c7c65c0116e2dbeb66b3084147171a7d62e6
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-11-18 23:07:10 +0530

    Create Grading Students.py

---
+++ Problem Solving/Algorithms/Implementation/Grading Students.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    g = []
+    for i in grades:
+        if i>=38:
+            a = i%5
+            if i%5 >= 3:
+                g.append(i+(5 -a))
+            else:
+                g.append(i)
+        else:
+            g.append(i)
+    return g
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

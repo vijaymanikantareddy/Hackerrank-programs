b42410773d221ccf7e94df11275f3d4c642c2af8
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-11-24 07:08:38 +0530

    Create Missing Numbers..py

---
+++ Problem Solving/Algorithms/Search/Missing Numbers..py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter 
+#
+# Complete the 'missingNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY brr
+#
+
+def missingNumbers(arr, brr):
+    # Write your code here
+    c1 = Counter(arr)
+    c2 = Counter(brr)
+    c = c2 - c1
+    return sorted(list(c.keys()))
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
